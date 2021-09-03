7fb8ea14f76a31efe733d97a58c4b42ee7bfe1a5
zed-chi <zed-chi@mail.ru>
2021-09-04 00:02:25 +0300

    lint

--- server.py
+++ server.py
@@ -1,19 +1,21 @@
-import trio
 import json
-from trio_websocket import serve_websocket, ConnectionClosed
+
+import trio
+from trio_websocket import ConnectionClosed, serve_websocket
 
 
 async def echo_server(request):
-    ws = await request.accept()    
+    ws = await request.accept()
     while True:
         try:
-            message =  await ws.get_message()
-            print(message)            
+            message = await ws.get_message()
+            print(message)
         except ConnectionClosed:
             break
 
 
 async def main():
-    await serve_websocket(echo_server, '127.0.0.1', 8080, ssl_context=None)
+    await serve_websocket(echo_server, "127.0.0.1", 8080, ssl_context=None)
+
 
 trio.run(main)
