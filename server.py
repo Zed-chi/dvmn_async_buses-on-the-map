762fb9be35739b79964a927392009b398768f7e6
zed-chi <zed-chi@mail.ru>
2021-09-03 20:51:09 +0300

    displayed busses on page

--- server.py
+++ server.py
@@ -1,16 +1,25 @@
 import trio
+import json
 from trio_websocket import serve_websocket, ConnectionClosed
 
 async def echo_server(request):
+    template = {
+        "msgType": "Buses",
+        "buses": [
+            {"busId": "c790сс", "lat": 55.7500, "lng": 37.600, "route": "120"},
+            {"busId": "a134aa", "lat": 55.7494, "lng": 37.621, "route": "670к"},
+        ]
+    }
     ws = await request.accept()
     while True:
         try:
             message = await ws.get_message()
-            await ws.send_message(message)
+            await ws.send_message(json.dumps(template))
         except ConnectionClosed:
             break
 
 async def main():
     await serve_websocket(echo_server, '127.0.0.1', 8000, ssl_context=None)
+
 
 trio.run(main)
