c30331816d53f0194645093ba82127517022e554
zed-chi <zed-chi@mail.ru>
2021-09-04 13:46:07 +0300

    added cycle func with skip parameter, and yielder func

--- fake_bus.py
+++ fake_bus.py
@@ -2,6 +2,7 @@ import itertools
 import json
 from sys import stderr
 from typing import Dict
+from random import randint
 
 import trio
 from trio_websocket import open_websocket_url
@@ -9,7 +10,7 @@ from trio_websocket import open_websocket_url
 from load_routes import load_routes
 
 URL = "ws://127.0.0.1:8080"
-
+BUSES_ON_ROUTE = 5
 
 async def bus_status_gen(bus_json):
     for lat, long in itertools.cycle(bus_json["coordinates"]):
@@ -21,9 +22,42 @@ async def bus_status_gen(bus_json):
         }
         await trio.sleep(5)
 
+def cycle(arr, start_id=0):
+    counter = start_id
+    while True:
+        if counter == len(arr)-1:
+            value = arr[counter]
+            counter = 0
+        else:
+            value = arr[counter]
+            counter += 1
+
+        yield value
+
+
+async def bus_status_gen2(bus_json):
+    buses_start_positions = list(
+        range(0, len(bus_json["coordinates"]),
+            len(bus_json["coordinates"]) // BUSES_ON_ROUTE)
+    )
+    bus_names = [f"{bus_json['name']}{randint(0,10000)}" for _ in range(BUSES_ON_ROUTE)]
+    bus_positions_gens = [
+        cycle(bus_json["coordinates"],start) for start in buses_start_positions
+    ]
+    while True:
+        for i in range(BUSES_ON_ROUTE):
+            position = next(bus_positions_gens[i])
+            yield {
+                "busId": bus_names[i],
+                "lat": position[0],
+                "lng": position[1],
+                "route": bus_json["name"],
+            }
+            await trio.sleep(2)
 
 async def run_bus(url: str, bus_id: str, route: Dict):
-    bus_status = bus_status_gen(route)
+    #bus_status = bus_status_gen(route)
+    bus_status = bus_status_gen2(route)
     while True:
         try:
             async with open_websocket_url(url) as ws:
