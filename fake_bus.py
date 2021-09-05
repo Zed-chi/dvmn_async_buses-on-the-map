4b30d7f07c349c05e4f0c8ce86aa79aa050e8dbd
zed-chi <zed-chi@mail.ru>
2021-09-06 00:51:57 +0300

    reorganized

--- fake_bus.py
+++ fake_bus.py
@@ -1,34 +1,20 @@
-import itertools
 import json
-from random import choice, randint
+import logging
+from random import randint
 from sys import stderr
-from typing import Dict
+from typing import Iterable
 
 import asyncclick as click
-# import click
 import trio
 from trio_websocket import open_websocket_url
 from trio_websocket._impl import ConnectionClosed, HandshakeError
 
 from load_routes import load_routes
 
-URL = "127.0.0.1:8080"
-BUSES_ON_ROUTE = 5
-INTERVAL_TIME = 1
+RECONNECT_INTERVAL = 5
 
 
-async def bus_status_gen(bus_json):
-    for lat, long in itertools.cycle(bus_json["coordinates"]):
-        yield {
-            "busId": bus_json["name"],
-            "lat": lat,
-            "lng": long,
-            "route": bus_json["name"],
-        }
-        await trio.sleep(5)
-
-
-def cycle(arr, start_id=0):
+def cycle(arr: Iterable, start_id: int = 0):
     counter = start_id
     while True:
         if counter == len(arr) - 1:
@@ -41,29 +27,37 @@ def cycle(arr, start_id=0):
         yield value
 
 
-async def bus_status_gen2(
-    refresh_timeout, emulator_id, buses_per_route, route_json
-) -> Dict:
-    buses_start_positions = list(
-        range(
-            0,
-            len(route_json["coordinates"]),
-            len(route_json["coordinates"]) // buses_per_route,
-        ),
+def get_positions_in_range(coordinates, bus_num):
+    step = max(1, len(coordinates) // bus_num)
+    length = len(coordinates)
+    return range(0, length, step)
+
+
+async def bus_info_generator(
+    refresh_timeout,
+    emulator_id,
+    buses_per_route,
+    route_json,
+):
+    buses_start_positions = get_positions_in_range(
+        route_json["coordinates"],
+        buses_per_route,
     )
+
     bus_names = [
-        f"{route_json['name']}{'-'+str(emulator_id)}{'-'+str(randint(0,10000))}"
+        f"{route_json['name']}{'-'+str(emulator_id) if emulator_id else ''}{'-'+str(randint(0,10000))}"
         for _ in range(buses_per_route)
     ]
-    bus_positions_gens = [
+    bus_position_generators = [
         cycle(route_json["coordinates"], start)
         for start in buses_start_positions
     ]
+
     while True:
-        for i in range(buses_per_route):
-            position = next(bus_positions_gens[i])
+        for idx in range(buses_per_route):
+            position = next(bus_position_generators[idx])
             yield {
-                "busId": bus_names[i],
+                "busId": bus_names[idx],
                 "lat": position[0],
                 "lng": position[1],
                 "route": route_json["name"],
@@ -79,32 +73,46 @@ async def run_bus(
     name,
     route,
 ):
-    bus_status = bus_status_gen2(
-        refresh_timeout, emulator_id, buses_per_route, route
+    bus_info = bus_info_generator(
+        refresh_timeout,
+        emulator_id,
+        buses_per_route,
+        route,
     )
     while True:
         try:
-            message = await bus_status.__anext__()
+            message = await bus_info.__anext__()
             await send_channel.send(message)
-
         except OSError as ose:
-            print("Connection attempt failed: %s" % ose, file=stderr)
+            logging.info("Connection attempt failed: %s" % ose, file=stderr)
+
+
+async def send_data_to_server(ws, receive_channel):
+    async for value in receive_channel:
+        await ws.send_message(json.dumps(value))
 
 
 async def send_to_server(url, receive_channel):
     while True:
         try:
             async with open_websocket_url(url) as ws:
-                print("connected")
-                async for value in receive_channel:
-                    await ws.send_message(json.dumps(value))
-
+                logging.info("connected to server")
+                async with trio.open_nursery() as nursery:
+                    nursery.start_soon(send_data_to_server, ws, receive_channel)
+                    nursery.start_soon(get_response_messages, ws)  
         except OSError as ose:
-            print("Connection attempt failed: %s" % ose, file=stderr)
+            logging.info("Connection attempt failed: %s" % ose, file=stderr)
         except (HandshakeError, ConnectionClosed) as e:
+            logging.info(e)
+            logging.info("waiting")
+            await trio.sleep(RECONNECT_INTERVAL)
+            logging.info("trying to reconnect to server")
 
-            print("waiting")
-            await trio.sleep(5)
+
+async def get_response_messages(ws):             
+    while True:
+        message = await ws.get_message()
+        logging.debug(message)    
 
 
 async def main(
@@ -116,6 +124,11 @@ async def main(
     refresh_timeout,
     v,
 ):
+    if v:
+        logging.basicConfig(level=logging.INFO)
+    else:
+        logging.basicConfig(level=logging.ERROR)
+
     routes = list(load_routes())
     if routes_number < len(routes) and routes_number != 0:
         routes = routes[:routes_number]
@@ -134,22 +147,26 @@ async def main(
                 route,
             )
 
-        for i in range(websockets_number):
+        for _ in range(websockets_number):
             nursery.start_soon(
-                send_to_server, ("ws://" + server), receive_channel
-            )
+                send_to_server,
+                ("ws://" + server),
+                receive_channel,
+            )        
 
 
 @click.command()
-@click.option("--server", default=URL, help="адрес сервера")
+@click.option("--server", default="127.0.0.1:8080", help="адрес сервера")
 @click.option("--routes_number", default=10, help="количество маршрутов")
 @click.option(
     "--buses_per_route",
-    default=BUSES_ON_ROUTE,
+    default=5,
     help="количество автобусов на каждом маршруте",
 )
 @click.option(
-    "--websockets_number", default=1, help="количество открытых веб-сокетов"
+    "--websockets_number",
+    default=1,
+    help="количество открытых веб-сокетов",
 )
 @click.option(
     "--emulator_id",
@@ -158,7 +175,7 @@ async def main(
 )
 @click.option(
     "--refresh_timeout",
-    default=INTERVAL_TIME,
+    default=1,
     help="задержка в обновлении координат сервера",
 )
 @click.option("--v", default=False, help="настройка логирования")
@@ -185,6 +202,3 @@ def run(
 
 if __name__ == "__main__":
     run()
-
-
-# todo loggging
