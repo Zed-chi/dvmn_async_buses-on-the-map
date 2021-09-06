7d7c1a0f74ad9e67cf83c897aefd56d5b65e06be
zed-chi <zed-chi@mail.ru>
2021-09-06 11:00:37 +0300

    changed checking type to isinstance

--- validators.py
+++ validators.py
@@ -6,16 +6,20 @@ def get_validated_map_frame(frame_json):
     for key in ["msgType", "data"]:
         if key not in frame_dict:
             raise ValueError(f"{key} is missing")
+
     if frame_dict["msgType"] != "newBounds":
         raise ValueError("Wrong msg type")
-    if not type(frame_dict["data"]) == dict:
+
+    if not isinstance(frame_dict["data"], dict):
         raise ValueError("'data' value type error")
     else:
-        for k, v in frame_dict["data"].items():
-            if k not in ["east_lng", "north_lat", "south_lat", "west_lng"]:
+        for key, value in frame_dict["data"].items():
+            if key not in ["east_lng", "north_lat", "south_lat", "west_lng"]:
                 raise ValueError("Invalid key in json[data]")
-            if type(v) != float:
+                
+            if not isinstance(value, float):
                 raise ValueError("Invalid position type")
+
     return frame_dict
 
 
@@ -29,12 +33,17 @@ def get_validated_bus_data(bus_json):
     ]:
         if key not in bus_dict:
             raise ValueError(f"{key} is missing")
-    if type(bus_dict["busId"]) != str:
+
+    if not isinstance(bus_dict["busId"], str):
         raise ValueError("busId type error")
-    if type(bus_dict["lat"]) != float:
+
+    if not isinstance(bus_dict["lat"], float):
         raise ValueError("lat type error")
-    if type(bus_dict["lng"]) != float:
+
+    if not isinstance(bus_dict["lng"], float):
         raise ValueError("lng type error")
-    if type(bus_dict["route"]) != str:
+
+    if not isinstance(bus_dict["route"], str):
         raise ValueError("route type error")
+
     return bus_dict
